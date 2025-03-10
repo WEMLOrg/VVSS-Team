f42b11babb862fab2dd8b33fbe1f3a03e218c466
Maria Felecan <159628578+mariafelecann@users.noreply.github.com>
2025-03-10 16:49:02 +0200

    modified name

--- Project_V2/src/app/MainApplication.java
+++ Project_V2/src/app/MainApplication.java
@@ -22,7 +22,7 @@ public class MainApplication {
         TemaValidator temaValidator = new TemaValidator();
         String filenameStudent = "fisiere/Studenti.xml";
         String filenameTema = "fisiere/Teme.xml";
-        String filenameNota = "fisiere/Note.xml";
+        String fn = "fisiere/Note.xml";
 
        //StudentFileRepository studentFileRepository = new StudentFileRepository(filenameStudent);
         //TemaFileRepository temaFileRepository = new TemaFileRepository(filenameTema);
@@ -32,7 +32,7 @@ public class MainApplication {
         StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
         TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
         NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
-        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(fn);
         Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
         UI ui = new UI(service);
         ui.run();
