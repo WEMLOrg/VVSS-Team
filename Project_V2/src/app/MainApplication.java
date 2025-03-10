902448b0dc08586e1066307c2e37a3ec15ee6299
Maria Felecan <159628578+mariafelecann@users.noreply.github.com>
2025-03-10 17:07:47 +0200

    name change

--- Project_V2/src/app/MainApplication.java
+++ Project_V2/src/app/MainApplication.java
@@ -20,7 +20,7 @@ public class MainApplication {
     public static void main(String[] args) {
         StudentValidator studentValidator = new StudentValidator();
         TemaValidator temaValidator = new TemaValidator();
-        String filenameStudent = "fisiere/Studenti.xml";
+        String fs = "fisiere/Studenti.xml";
         String filenameTema = "fisiere/Teme.xml";
         String fileNmaeNota = "fisiere/Note.xml";
 
@@ -29,7 +29,7 @@ public class MainApplication {
         //NotaValidator notaValidator = new NotaValidator(studentFileRepository, temaFileRepository);
         //NotaFileRepository notaFileRepository = new NotaFileRepository(filenameNota);
 
-        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(fs);
         TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
         NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
         NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
